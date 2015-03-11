9774ee76498e732402b18a88464cd2cdd9c2ee6f
hiphip90 <rvashurin@gmail.com>
2015-03-11 21:23:48 +0300

    Test then implement Answer model validations

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_db_column(:body).of_type(:text) }
+  it { should validate_presence_of(:body) }
 end
