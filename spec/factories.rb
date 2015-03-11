54e37c11d49611701dbc82a85daae4feea6e08d0
hiphip90 <rvashurin@gmail.com>
2015-03-11 21:20:54 +0300

    Generate answer model, run migration, configure factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,10 @@
-FactoryGirl.define do
+FactoryGirl.define do  
   factory :question do
     title "My question"
     body "Ultimate Question of Life, Universe and Everything"
+  end
+
+  factory :answer do
+    body "42"
   end
 end
