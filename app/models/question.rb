6197290a5f6159c088563fe3b4dc2cba1c26a910
hiphip90 <rvashurin@gmail.com>
2015-03-11 21:18:41 +0300

    Test then implement Question model validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ActiveRecord::Base
+  validates :title, :body, presence: true
+  validates :title, length: { maximum: 150 }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  
+  it { should have_db_column(:title).of_type(:string) }
+  it { should have_db_column(:body).of_type(:text) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_length_of(:title).is_at_most(150) }
 end
