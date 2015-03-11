43e9ef3777c2e405a272dc92d3fedc3bdd412dd8
hiphip90 <rvashurin@gmail.com>
2015-03-11 21:40:00 +0300

    Test then implement association between questions and answers

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should have_db_column(:body).of_type(:text) }
   it { should validate_presence_of(:body) }
+  it { should belong_to(:question) }
 end
