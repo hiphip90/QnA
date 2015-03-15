8960b0ffe86e8ad3945266bd2f79e89b77376c25
hiphip90 <rvashurin@gmail.com>
2015-03-15 19:20:30 +0300

    Set up seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@example.com"
+  User.create!(name: name, email: email)
+end
+
+User.all.each do |user|
+  title = Faker::Lorem.sentence(6)
+  body = Faker::Lorem.paragraph(4)
+  user.questions.create!(title: title, body: body)
+end
+
+Question.all.each do |question|
+  User.all.each do |user|
+    body = Faker::Lorem.sentence(6)
+    question.answers.create!(body: body, user_id: user.id)
+  end
+end
