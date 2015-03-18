1cfca9d162a9b8a0e2b2439b836d5f7b939602a3
hiphip90 <rvashurin@gmail.com>
2015-03-18 11:58:08 +0300

    Cover questions list with feature spec

---
+++ spec/features/question_list_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'Checking questions list', %q{
+  In order to check if question has already been asked
+  As a user
+  I must be able to see questions list
+} do
+
+  given!(:question_list) { create_list(:question, 3) }
+
+  scenario 'User requests questions list' do
+    visit root_path
+    click_on 'Questions'
+    question_list.each do |question|
+      expect(page).to have_content question.title
+    end
+  end
+end
