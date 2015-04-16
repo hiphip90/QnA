b0f38b69e5daf089892b1c1069376c486a9aae5c
hiphip90 <rvashurin@gmail.com>
2015-04-16 21:38:51 +0300

    Refactor accept answer process

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,13 +5,9 @@ class Question < ActiveRecord::Base
   validates :title, :body, presence: true
   validates :title, length: { maximum: 150 }
 
-  def has_accepted_answer?
-    answers.where("accepted = ?", true).any?
-  end
-
   def accept(answer)
     return false unless answer.question_id == self.id
-    answers.where("accepted = ?", true).first.update(accepted: false) if self.has_accepted_answer?
+    answers.where("accepted = ?", true).update_all(accepted: false)
     answer.update(accepted: true)
   end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -10,24 +10,6 @@ RSpec.describe Question, type: :model do
   it { should have_many(:answers).dependent(:destroy) }
   it { should belong_to(:user) }
 
-  describe '#has_accepted_answer?' do
-    let(:question) { create(:question, :with_answers) }
-    let(:answer) { question.answers.last }
-
-    context 'when there is an accepted answer' do
-      it 'returns true' do
-        answer.update_attributes(accepted: true)
-        expect(question.has_accepted_answer?).to be_truthy
-      end
-    end
-
-    context 'when there is no accepted answer' do
-      it 'returns false' do
-        expect(question.has_accepted_answer?).to be_falsey
-      end
-    end
-  end
-
   describe '#accept' do
     let(:question) { create(:question, :with_answers) }
     let(:other_question) { create(:question) }
