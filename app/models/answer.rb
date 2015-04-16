b04e6e56d7ca94475249ba5be01ea46b2d0257bb
hiphip90 <rvashurin@gmail.com>
2015-04-16 23:08:04 +0300

    Refactor answer creation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,7 +2,7 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
   validates :body, presence: true
-  default_scope { order('accepted DESC') }
+  default_scope { order('accepted DESC, created_at DESC') }
 
   def accept
     question.answers.where("accepted = ?", true).update_all(accepted: false)
