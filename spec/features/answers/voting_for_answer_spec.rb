52d938b67db244be0aaf3a964e6fdcf46678045c
hiphip90 <rvashurin@gmail.com>
2015-05-01 10:13:39 +0300

    Write feature spec for answer voting, config routes, add links

---
+++ spec/features/answers/voting_for_answer_spec.rb
@@ -0,0 +1,59 @@
+require_relative '../feature_js_config'
+
+feature 'Voting for answer', %q{
+  In order to express my opinion
+  As an authenticated user
+  I must be able to vote for answer
+} do
+
+  let(:user) { create(:user) }
+  let(:question) { create(:question) }
+  let!(:answer) { create(:answer, question: question) }
+  
+  background do
+    sign_in_as(user)
+  end
+  
+  scenario 'User upvotes answer', js:true do
+    visit question_path(question)
+
+    within ('.answers') do
+      click_link 'Upvote'
+      expect('.rating').to have_content '1'
+    end
+  end
+
+  scenario 'User downvotes answer', js:true do
+    visit question_path(question)
+
+    within ('.answers') do
+      click_link 'Downvote'
+      expect('.rating').to have_content '-1'
+    end
+  end
+
+  scenario 'User can vote only once', js:true do
+    visit question_path(question)
+
+    within ('.answers') do
+      click_link 'Downvote'
+
+      expect(page).to_not have_content('Upvote')
+      expect(page).to_not have_content('Downvote')
+    end
+  end
+
+  scenario 'User recalls his vote', js:true do
+    visit question_path(question)
+
+    within ('.answers') do
+      click_link 'Downvote'
+      expect('.rating').to have_content '-1'
+
+      click_link 'Recall vote'
+      expect('.rating').to have_content '0'
+      expect(page).to have_content('Upvote')
+      expect(page).to have_content('Downvote')
+    end
+  end
+end
