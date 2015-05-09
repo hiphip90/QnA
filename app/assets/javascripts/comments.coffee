e356fffa2cafb2e4d6e1e692ded0e4764af33f6c
hiphip90 <rvashurin@gmail.com>
2015-05-09 10:00:36 +0300

    Add commenting to answer

--- app/assets/javascripts/comments.coffee
+++ app/assets/javascripts/comments.coffee
@@ -2,11 +2,23 @@ $ ->
   questionId = $('.question').data('questionId');
 
   $('.new-question-comment-link').click (e) ->
+    e.preventDefault()
     $('.question-comment-form').show();
+
+  $('.new-answer-comment-link').click (e) ->
+    e.preventDefault()
+    answer = $(this).parents('li');
+    answer.find('.answer-comment-form').show();
 
   # process new comment creation
   Danthes.subscribe "/questions/#{questionId}/comments", (data, channel) -> 
     comment = $.parseJSON(data.comment);
-    $('.comment-errors').remove();
-    $('.comments-block').append(JST["templates/_comment"]({ comment: comment }));
-    $('#new_comment textarea').val('');
+    if comment.commentable_type == 'Answer'
+      answer = $("#answer_#{comment.commentable_id}")
+      answer.find('.comment-errors').remove()
+      answer.find('.comments-block').append(JST["templates/_comment"]({ comment: comment }));
+      $('#new_comment textarea').val('');
+    else
+      $('.question .comment-errors').remove();
+      $('.question .comments-block').append(JST["templates/_comment"]({ comment: comment }));
+      $('#new_comment textarea').val('');
--- spec/features/comments/create_comment_spec.rb
+++ spec/features/comments/create_comment_spec.rb
@@ -8,7 +8,7 @@ feature 'Create comment', %q{
 
   given(:user) { create(:user, :author) }
   given(:question) { user.questions.last }
-  given(:answer) { create(:answer, question: question) }
+  given!(:answer) { create(:answer, question: question) }
 
   background do
     sign_in_as(user)
@@ -26,6 +26,29 @@ feature 'Create comment', %q{
 
       expect(page).to have_content "Commentin'"
       expect(other_user_session).to have_content "Commentin'"
+    end
+
+    within ".answers-block" do
+      expect(page).to_not have_content "Commentin'"
+    end
+  end
+
+  scenario 'add comment to answer', js: true do
+    other_user_session = Capybara::Session.new(:webkit, QnA::Application)
+    other_user_session.visit question_path(user.questions.last)
+    visit question_path(question)
+
+    within ".answers-block" do
+      click_link 'Add comment'
+      fill_in 'Comment', with: "Commentin'"
+      click_on 'Post'
+
+      expect(page).to have_content "Commentin'"
+      expect(other_user_session).to have_content "Commentin'"
+    end
+
+    within ".question" do
+      expect(page).to_not have_content "Commentin'"
     end
   end
 end
